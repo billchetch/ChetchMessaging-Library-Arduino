259bff49a4b0c038891d745b4178270b9044510e
billchetch <45006134+billchetch@users.noreply.github.com>
2021-10-03 10:21:37 +0800

    s

--- examples/esp8266_bridge/esp8266_bridge.ino
+++ examples/esp8266_bridge/esp8266_bridge.ino
@@ -1,10 +1,13 @@
 #include <ChetchNetworkAPI.h>
 #include <ChetchWifiManager.h>
+#include <ChetchStreamFlowController.h>
+#include <ChetchADM.h>
+
 
 //Normally only need to change these
-#define TRACE false
+#define TRACE true
 #define HOSTNAME "oblong3" //this identifies the board on the network so needs to be changed on a per board basis
-#define PORT 8088 //doesn't need to be changed
+#define PORT 8090 //IMPORTANT: this should be unique with the HOSTNAME as well (starting at 8088 and going up) ... this is to prevent confusion if using dynamic dns
 
 //Network credentials
 /*
@@ -13,45 +16,90 @@
 #define NETWORK_SERVICE_URL "http://192.168.1.188:8001/api/service"
 */
 
-#define SSID "Bulan Baru"
+/*#define SSID "Bulan Baru"
+#define PASSWORD "bbcharters"
+#define NETWORK_SERVICE_URL "http://192.168.2.100:8001/api/service"*/
+
+#define SSID "Bulan Baru KM"
 #define PASSWORD "bbcharters"
 #define NETWORK_SERVICE_URL "http://192.168.2.100:8001/api/service"
 
 using namespace Chetch;
 
 WiFiServer server(PORT);
+WiFiClient client;
+bool serviceRegistered = false;
+
+void onWifiEvent(WifiManager::WifiEvent event){
+  switch(event){
+    case WifiManager::WifiEvent::CONNECTED:
+      break;
+      
+    case WifiManager::WifiEvent::GOT_IP:
+      break;
+      
+    case WifiManager::WifiEvent::DISCONNECTED:
+      sendCommand(&Serial, ArduinoDeviceManager::RESET_ADM_COMMAND);
+      
+      if(TRACE){
+        Serial.println("Disconnected... sending Reset ADM command byte to Arduino");
+      }
+      break;
+  }
+}
+
+void sendCommand(Stream* stream, byte cmd){
+  stream->write(StreamFlowController::COMMAND_BYTE);
+  stream->write(cmd);
+}
+
+void sendEvent(Stream* stream, byte evt){
+  stream->write(StreamFlowController::EVENT_BYTE);
+  stream->write(evt);
+}
+
+bool registerService(){
+  while(!WifiManager::hasIP()){
+    delay(500);
+    if(TRACE)Serial.println("Waiting for IP...");
+  }
+  NetworkAPI::trace = TRACE;
+  serviceRegistered = NetworkAPI::registerService(NETWORK_SERVICE_URL, HOSTNAME, PORT);
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(100);
+
+  //this will connect to the network (or timeout if provided)
   WifiManager::trace = TRACE;
-  WifiManager::connect(HOSTNAME, SSID, PASSWORD);
+  WifiManager::connect(HOSTNAME, SSID, PASSWORD, onWifiEvent);
+  delay(100);
   
-  //now register with network service
-  delay(1000);
-  NetworkAPI::trace = TRACE;
-  NetworkAPI::registerService(NETWORK_SERVICE_URL, HOSTNAME, PORT);
-  
-  //start server for TCP connections
+  //now we register service ... firstly it waits for an IP to be available and then we set Network api with an infinte timeout ae 
+  //we need the service to register for any client to be able to connect (i.e. this is all useless unless it regisers)
+  registerService();
+  delay(100);
+
+  //start up server
   server.begin();
-  if(TRACE){
-    Serial.print("Connect to this device @: ");
-    Serial.print(WiFi.localIP());
-    Serial.print(":");
-    Serial.println(PORT);
-  }
-  
+  delay(100);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  WiFiClient client = server.available();
+  //Ideally we'd respond to a lack of IP (resulting from a disconnect event) in the event handler however that was causing
+  //an exception so we check here
+  if(!WifiManager::hasIP()){
+    //registerService will wait until an IP is available before continuing
+    registerService();
+  }
   
+  // put your main code here, to run repeatedly:
+  client = server.available(); 
   if (client) {
     client.setNoDelay(true);
-    
     while(client.connected()){ 
-
       //read from client (wifi) and forward to serial (arduino)
       while(client.available()){
         Serial.write(client.read());
